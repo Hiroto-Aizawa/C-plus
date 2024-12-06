ac4a51eea929bcb6f9fde752066dcfe9a88776e6
Hiroto-Aizawa <r_20love@icloud.com>
2024-12-07 02:43:44 +0900

    1_10_xtra 追加

---
+++ Chapter1/1_10_xtra.cpp
@@ -0,0 +1,68 @@
+// https://atcoder.jp/contests/apg4b/tasks/APG4b_cm
+
+
+#pragma region  提出版
+
+#include "bits/stdc++.h"
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+    int i = 1;
+    // ここにプログラムを追記
+    cout << "A:";
+    if(1 <= A){
+        while(i <= A){
+            cout << "]";
+            i++;
+        }
+    }
+    cout << endl;
+
+    i=0;
+
+    cout << "B:";
+    if(1 <= B){
+        while(i <= B){
+            cout << "]";
+            i++;
+        }
+    }
+    cout << endl;
+}
+
+#pragma endregion
+
+
+
+#pragma region 解答例
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+
+    int i = 0;
+    cout << "A:";
+    //入力された値が0の場合の条件式は、
+    //
+    while (i < A) {
+        cout << "]";
+        i++;
+    }
+    cout << endl;
+
+    i = 0;
+    cout << "B:";
+    while (i < B) {
+        cout << "]";
+        i++;
+    }
+    cout << endl;
+}
+
+
+#pragma endregion
