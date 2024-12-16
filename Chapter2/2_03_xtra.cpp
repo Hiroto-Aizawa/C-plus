7fd16840b784db1d9b636e5bc791898a9b4f22a6
Hiroto-Aizawa <r_20love@icloud.com>
2024-12-16 21:19:55 +0900

    2_03 更新

--- Chapter2/2_03_xtra.cpp
+++ Chapter2/2_03_xtra.cpp
@@ -2,12 +2,22 @@
 
 // 【ゲーム大会 】
 
+/*
+問題文
+あるゲーム大会にはN人が参加しM試合が行われました。各参加者には1からNの番号が割り当てられています。
+試合に関する情報が与えられるので、M回の試合がすべて終了した時点での試合結果の表を作成し、出力してください。
+
+ただし、同じ参加者のペアについて2回以上試合が行われることはないとします。
+
+*/
+
 #pragma region 提出版
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
+    // 人数=N、試合数=M
     int N, M;
     cin >> N >> M;
     vector<int> A(M), B(M);
@@ -22,7 +32,8 @@ int main() {
     vector<vector<char>> table(N, vector<char>(N, '-'));
 
     for (int i = 0; i < M; i++) {
-        // 1〜N → 0〜N-1 に変換
+        // 問題ではR1,2=o、R2,1=xとなるが、配列は0から始まるので、
+        // 1〜N → 0〜N-1 に変換する必要がある
         A[i]--;
         B[i]--;
         table[A[i]][B[i]] = 'o';  // AはBに勝った
