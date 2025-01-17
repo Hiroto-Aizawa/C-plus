e37e6f3e25ae3708d713ea3cdd318cdaddb21aaa
Hiroto-Aizawa <r_20love@icloud.com>
2025-01-17 17:38:23 +0900

    AtCoder用テンプレートスクリプト作成

---
+++ Template.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+//-------------------------------------------
+// #include <vector>
+// #include <algorythm>
+using namespace std;
+
+/* マクロ */
+#define rep(i, a, n) for (int i=a; i < n; i++)
+
+// 桁和を求める関数
+int digit_sum(int num) {
+    int sum = 0;
+
+    while(num) {
+        sum += n % 10;
+        n /= 10;
+    }
+
+    return num;
+}
+
+#pragma region main
+
+int main() {
+    int n;
+    cin >> n;
+
+    return 0;
+}
+
+#pragma endregion
