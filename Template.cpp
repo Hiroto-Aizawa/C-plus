18d69731c5f8145258f42858a9eb1b99eecd7fcb
Hiroto-Aizawa <r_20love@icloud.com>
2025-01-17 20:24:23 +0900

    テンプレート更新（define追加、digit_sum関数修正、頻出の入力追加）

--- Template.cpp
+++ Template.cpp
@@ -5,26 +5,38 @@
 using namespace std;
 
 /* マクロ */
+// 関数
 #define rep(i, a, n) for (int i=a; i < n; i++)
+#define REP(i, a, n) for (int i=a; i <= n; i++)
+// 出力系
+#define Yes cout << "Yes" << endl
+#define No cout << "No" << endl
+#define YES cout << "YES" << endl
+#define NO cout << "NO" << endl
 
 // 桁和を求める関数
-int digit_sum(int num) {
+int digit_sum(int n) {
     int sum = 0;
 
-    while(num) {
+    while(n) {
         sum += n % 10;
         n /= 10;
     }
-
-    return num;
+    return sum;
 }
 
 #pragma region main
 
 int main() {
-    int n;
-    cin >> n;
-
+    // int n;
+    // cin >> n;
+    
+    // string s;
+    // cin >> s;
+    
+    // vector<int> a(n);
+    // rep(i, 0, n) cin >> a[i];
+    
     return 0;
 }
 
